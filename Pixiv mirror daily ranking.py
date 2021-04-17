d5e0a3259e586d54a59d0a0b22a68b9495af24a7
TMFfa <81461670+TMFfa@users.noreply.github.com>
2021-04-17 17:40:19 +0800

    enable to customize date

--- Pixiv mirror daily ranking.py
+++ Pixiv mirror daily ranking.py
@@ -26,8 +26,8 @@ except Exception as e:
 
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.68'
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                  ' Chrome/89.0.4389.128 Safari/537.36 Edg/89.0.774.77'
 }
 
 
@@ -54,22 +54,57 @@ def geturls():
         url = 'https://www.vilipix.com/api/illust?mode=daily&date={date}&limit=30&offset={p}' \
             .format(date=date, p=str(page * 30))
         r = requests.get(url, headers=headers)
-        if r.text == '{"count":54,"rows":[]}':
+        js = json.loads(r.text)
+        if not js['rows']:
             return urls
         else:
             urls.append(url)
             page += 1
 
 
-def main():
+def detailurls(date):
+    page = 0
+    urls = []
+    while True:
+        url = 'https://www.vilipix.com/api/illust?mode=daily&date={date}&limit=30&offset={p}' \
+                .format(date=date, p=str(page * 30))
+        r = requests.get(url, headers=headers)
+        js = json.loads(r.text)
+        if not js['rows']:
+            return urls
+        else:
+            urls.append(url)
+            page += 1
+
+
+def yesterday_ranking():
     start = datetime.datetime.now()
     for url in geturls():
         try:
             download(url)
-        except Exception as ee:
-            print(ee)
+        except Exception as e:
+            print(e)
     end = datetime.datetime.now()
     print('totally used ', (end-start).seconds, ' seconds')
+
+
+def detail_daily_ranking(date):
+    start = datetime.datetime.now()
+    for url in detailurls(date):
+        try:
+            download(url)
+        except Exception as e:
+            print(e)
+    end = datetime.datetime.now()
+    print('totally used ', (end-start).seconds, ' seconds')
+
+
+def main():
+    choice = input('请选择下载模式，回车下载昨日日榜，\n输入日榜日期（格式如：20210406）下载指定日期日榜：')
+    if choice == '':
+        yesterday_ranking()
+    else:
+        detail_daily_ranking(choice)
 
 
 if __name__ == '__main__':
